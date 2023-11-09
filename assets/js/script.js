9ee8028b6daef8c2b89486a002966ee5e73efad4
kimura12345678 <142760405+kimura12345678@users.noreply.github.com>
2023-11-10 01:04:52 +0700

    feat(html,css,js): add special dish section and menu section

--- assets/js/script.js
+++ assets/js/script.js
@@ -116,3 +116,25 @@ addEventOnElements([heroSliderNextBtn, heroSliderPrevBtn], "mouseover", function
 addEventOnElements([heroSliderNextBtn, heroSliderPrevBtn], "mouseout", autoSlide);
 
 window.addEventListener("load", autoSlide);
+
+//PARALLAX EFFECT
+const parallaxItems = document.querySelectorAll("[data-parallax-item]");
+
+let x, y;
+
+window.addEventListener("mousemove", function (event) {
+
+    x = (event.clientX / window.innerWidth * 10) - 5;
+    y = (event.clientY / window.innerHeight * 10) - 5;
+
+    // reverse the number eg. 20 -> -20, -5 -> 5
+    x = x - (x * 2);
+    y = y - (y * 2);
+
+    for (let i = 0, len = parallaxItems.length; i < len; i++) {
+        x = x * Number(parallaxItems[i].dataset.parallaxSpeed);
+        y = y * Number(parallaxItems[i].dataset.parallaxSpeed);
+        parallaxItems[i].style.transform = `translate3d(${x}px, ${y}px, 0px)`;
+    }
+
+});
